d9891f2cb6302b0b91c1155942c17740cf11b3f2
henriqueyun <henriqueyun@gmail.com>
2021-06-15 00:31:19 -0300

    adicionando git history nas skills

---
+++ js/git-history.js
@@ -0,0 +1,27 @@
+function buildGitHistoryTable (repos) {
+  let table = document.querySelector('.git-history-table')
+  repos.forEach((repo, i) => {
+    let repoLine = table.insertRow(1);
+    let repoName = repoLine.insertCell(0);
+    let repoLang = repoLine.insertCell(1);
+    repoName.innerHTML = repo.name
+    repoLang.innerHTML = repo.language || 'Não há'
+  });
+}
+
+function getGitReposHistory (callback) {
+  let repos;
+  let reposReq = new XMLHttpRequest();
+  reposReq.open('GET', 'https://api.github.com/users/henriqueyun/repos');
+  reposReq.send();
+  reposReq.onload = function(e) {
+    repos = JSON.parse(e.currentTarget.responseText);
+    // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#descri%C3%A7%C3%A3o
+    repos.sort((repoA, repoB) => {
+      return Date.parse(repoB.updated_at) - Date.parse(repoA.updated_at);
+    })
+    callback(repos)
+  };
+}
+
+getGitReposHistory(buildGitHistoryTable)
